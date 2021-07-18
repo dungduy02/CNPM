be0b3c538ec0aef7e0f657dfb66a1346a16dfabe
dungduy02 <18130048@st.hcmuaf.edu.vn>
2021-07-18 19:57:53 +0700

    complate

--- src/main/java/nlu/edu/fit/bookstore/connection/DBconnection.java
+++ src/main/java/nlu/edu/fit/bookstore/connection/DBconnection.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBconnection {
-    private static  String USER ="iVE4WWbt9c";
-    private static String password = "hq5acwl1YC";
-    private static String hostname = "remotemysql.com";
-    private static String dbname = "iVE4WWbt9c";
+    private static  String USER ="sql5426179";
+    private static String password = "3TbIT6xZVq";
+    private static String hostname = "sql5.freesqldatabase.com";
+    private static String dbname = "sql5426179";
     private static String connectionURL = "jdbc:mysql://" + hostname + ":3306/" + dbname;
 
 
--- src/main/java/nlu/edu/fit/bookstore/utils/Utils.java
+++ src/main/java/nlu/edu/fit/bookstore/utils/Utils.java
@@ -8,12 +8,12 @@ import java.sql.SQLOutput;
 public class Utils {
     public static String fullPath(String path) {
 
-        return "https://jb-bookstore.herokuapp.com/" + path;
+        return "https://bookstores-jb.herokuapp.com/" + path;
     }
 
     public static String fullPathAdmin(String path) {
 
-        return "https://jb-bookstore.herokuapp.com/" + path;
+        return "https://bookstores-jb.herokuapp.com/" + path;
     }
 
     public static boolean isEmail(String email) {
